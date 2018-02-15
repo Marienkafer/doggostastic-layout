19693b1361359bca9e7e4323e7310ecb6c5f3598
Marienkafer <marienbsequeram@gmail.com>
2018-02-15 00:34:13 -0300

    creating basic structure and behaviour for doggotastic webpage

---
+++ js/behaviour.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#add-bttn").click(function(){
+        $("#add-form").show('fast');
+    });
+});
+
+$(document).ready(function(){
+    $("#submit-bttn").click(function(){
+        $("#add-form").hide('fast');
+    });
+});
+
